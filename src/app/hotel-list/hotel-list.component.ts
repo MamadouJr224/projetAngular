b3f7cdbcbcf63265539a53bbf1b4917a56bed62b
mamadou <dianemamadoucire36@gmail.com>
2022-04-23 04:50:54 +0200

    d

--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -1,11 +1,12 @@
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {IHotel} from "./hotel";
 @Component({
   selector: 'app-hotel-list',
-  templateUrl: './hotel-list.component.html'
+  templateUrl: './hotel-list.component.html',
+  styleUrls: ['./hotel-list.component.css']
 })
 
-export class HotelListComponent{
+export class HotelListComponent implements OnInit {
   public  title = 'liste hotels';
   public hotels: IHotel[] = [
     {
@@ -38,9 +39,37 @@ export class HotelListComponent{
     }
   ];
   public showBadge: boolean | undefined;
-  public hotelFilter = 'mot';
+  private _hotelFilter = 'mot' ;
+  public filteredHotels: IHotel[] = [];
+
+  ngOnInit() {
+    this.filteredHotels = this.hotels;
+    this.hotelFilter = 'mot';
+  }
 
   public toggleIsNewBadge(): void {
     this.showBadge = !this.showBadge;
   }
+  public get hotelFilter(): string {
+    return this._hotelFilter;
+  }
+
+   public set hotelFilter(filter: string){
+    this._hotelFilter = filter;
+
+    this.filteredHotels = this.hotelFilter ? this.filterHotels(this.hotelFilter): this.hotels;
+  }
+
+
+  private filterHotels(criteria: string): IHotel[]{
+    criteria = criteria.toLocaleLowerCase();
+
+    const res = this.hotels.filter(
+      (hotel:IHotel) => hotel.hotelName.toLocaleLowerCase().indexOf(criteria) !== -1
+    );
+
+    return res;
+
+  }
 }
+
