48e5a51017128d9ee44efe6e110e0d426845ca5f
Mamadou cire Diane <dianemamadoucire36@gmail.com>
2023-07-26 14:33:21 +0200

    8

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HotelListComponent } from './hotel-list/hotel-list.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
+        HotelListComponent // Assurez-vous d'ajouter le composant ici
       ],
     }).compileComponents();
   });
